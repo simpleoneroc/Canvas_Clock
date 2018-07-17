0eeceb0ac780b994b36748c4f0c3e83479178032
simpleone_roc <simpleoneroc@users.noreply.github.com>
2018-07-18 00:27:23 +0800

    Canvas_clock1.1

---
+++ countdown.js
@@ -0,0 +1,38 @@
+var WINDOW_WIDTH = 1024,
+    WINDOW_HEIGHT = 768,
+    RADIUS = 8;
+
+window.onload = function(){
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+
+    canvas.width = WINDOW_WIDTH;
+    canvas.height = WINDOW_HEIGHT;
+
+    render(context);
+}
+
+function render(cxt){
+    var hours = 12,
+        minutes = 34,
+        seconds = 56;
+    
+    renderDigit(0,0,parseInt(hours/10),cxt);
+}
+
+function renderDigit(x,y,num,cxt){
+
+    cxt.fillStyle = "rgb(0,102,153)";
+
+    for (let i = 0; i < digit[num].length; i++){
+        for (let j = 0; j < digit[num][i].length; j++){
+
+            if(digit[num][i][j] == 1){
+                cxt.beginPath();
+                cxt.arc(x + j * 2 * (RADIUS + 1) + (RADIUS + 1), y + i * 2 *(RADIUS + 1) + (RADIUS + 1), RADIUS, 0, 2*Math.PI);
+                cxt.closePath();
+                cxt.fill();
+            }
+        }
+    }
+}
